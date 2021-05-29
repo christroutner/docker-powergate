40bbce4dd3a654ea0c33fb7dbdb01f60bbdee7f9
Chris Troutner <chris.troutner@gmail.com>
2021-05-28 19:51:26 -0700

    Tested shell script for building lotus docker container

--- lotus-image/build-lotus-image.sh
+++ lotus-image/build-lotus-image.sh
@@ -6,6 +6,15 @@
 sudo apt update
 sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev wget -y && sudo apt upgrade -y
 
+# Install Rust and rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+# Install Go
+wget -c https://golang.org/dl/go1.16.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc && source ~/.bashrc
+
+
 # Clone the Textile lotus build repo.
 git clone https://github.com/textileio/lotus-build
 cd lotus-build
@@ -20,4 +29,4 @@ RUSTFLAGS="-C target-cpu=native" FFI_BUILD_FROM_SOURCE=1 make deps
 
 # Build the Docker image
 cd ..
-docker build -t lotus:trout
+docker build -t lotus:trout .
